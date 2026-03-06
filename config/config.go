6da3a7c98e7932fe7ad6e09f8aeede692c2e4166
Achilles80 <adithyaajay2005@gmail.com>
2026-03-06 09:02:51 +0530

    .

--- config/config.go
+++ config/config.go
@@ -10,16 +10,16 @@ import (
 // Set each node's IP to the machine that will run it.
 // Use "127.0.0.1" for all when testing locally on a single machine.
 var NodeHosts = map[int]string{
-	1: "192.168.0.113", // Machine A — Cluster Manager Node 1
-	2: "192.168.0.113", // Machine B — Cluster Manager Node 2
-	3: "192.168.100.3", // Machine C — Cluster Manager Node 3
+	1: " 172.26.90.254", // Machine A — Cluster Manager Node 1
+	2: " 172.26.90.254", // Machine B — Cluster Manager Node 2
+	3: "172.26.90.4", // Machine C — Cluster Manager Node 3
 }
 
 // TokenManagerHost is the IP of the machine running the Token Manager.
-const TokenManagerHost = "192.168.0.113"
+const TokenManagerHost = " 172.26.90.254"
 
 // DeadlockDetectorHost is the IP of the machine running the Deadlock Detector.
-const DeadlockDetectorHost = "192.168.0.113"
+const DeadlockDetectorHost = " 172.26.90.254"
 
 // Base ports for different components
 const (
